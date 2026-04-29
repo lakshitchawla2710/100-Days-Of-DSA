a0e689c27d62be71ba42dc7164259b8a863083a0
lakshitchawla27 <lakshit.chawla.27@gmail.com>
2026-04-29 12:00:00 +0530

    Day088: DSA Practice

---
+++ Day088 q001.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Day088 - Question 1
+// DSA Practice
+
+int main() {
+    printf("Day088 - Q1 Solution\n");
+    return 0;
+}
---
+++ Day088 q002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Day088 - Question 2
+// DSA Practice
+
+int main() {
+    printf("Day088 - Q2 Solution\n");
+    return 0;
+}

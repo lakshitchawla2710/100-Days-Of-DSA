555ef026e79a9dca0e3d073b50faba302667bdeb
lakshitchawla27 <lakshit.chawla.27@gmail.com>
2026-05-01 12:00:00 +0530

    Day090: DSA Practice

---
+++ Day090 q001.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Day090 - Question 1
+// DSA Practice
+
+int main() {
+    printf("Day090 - Q1 Solution\n");
+    return 0;
+}
---
+++ Day090 q002.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Day090 - Question 2
+// DSA Practice
+
+int main() {
+    printf("Day090 - Q2 Solution\n");
+    return 0;
+}
